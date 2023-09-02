2cadd27805f5407744eda8d2eee284d19ba878c5
Leonardo Asitimbaya <galo.asitimbaya@epn.edu.ec>
2023-09-01 19:21:46 -0500

    feat: componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarritoComponent } from './pantallas/carrito/carrito.component';
+import { CatalogoComponent } from './pantallas/catalogo/catalogo.component';
+import { DetalleComponent } from './pantallas/detalle/detalle.component';
+import { InicioComponent } from './pantallas/inicio/inicio.component';
+import { NosotrosComponent } from './pantallas/nosotros/nosotros.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarritoComponent,
+    CatalogoComponent,
+    DetalleComponent,
+    InicioComponent,
+    NosotrosComponent,
+    CabeceraComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
