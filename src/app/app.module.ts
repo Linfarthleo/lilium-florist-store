51f86c38159f7c940ca67de1199c1e6a691ad05d
Karla Vivas <karla.vivas@epn.edu.ec>
2023-09-07 16:33:44 -0500

    Material Angular agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { InicioComponent } from './pantallas/inicio/inicio.component';
 import { NosotrosComponent } from './pantallas/nosotros/nosotros.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
