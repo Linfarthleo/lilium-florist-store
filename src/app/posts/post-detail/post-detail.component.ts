86ef88dcaa08feac3e612b21be6135806a507d21
Leonardo Asitimbaya <galo.asitimbaya@epn.edu.ec>
2023-09-12 17:15:11 -0500

    feat: post actualizados

--- src/app/posts/post-detail/post-detail.component.ts
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-post-detail',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-detail.component.scss']
 })
 export class PostDetailComponent {
-
+  @Input() title!: string;
+  @Input() subtitle!: string;
+  @Input() imageSrc!: string;
+  @Input() content!: string;
 }
